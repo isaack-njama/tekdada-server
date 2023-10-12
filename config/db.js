9d056bf7d61e959337cb17f47d3b51ee45075df2
Isaack Njama <munguiisaack@gmail.com>
2023-10-12 23:53:43 +0300

    feat: configure db connection

---
+++ config/db.js
@@ -0,0 +1,10 @@
+import "dotenv/config";
+
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const mongoDB = process.env.MONGODB_URI;
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
