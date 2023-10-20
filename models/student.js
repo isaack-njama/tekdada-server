95e9270f1736c3e0565cfe9f05c27e3160e131e8
Isaack Njama <munguiisaack@gmail.com>
2023-10-20 10:02:12 +0300

    chore: modify import

--- models/student.js
+++ models/student.js
@@ -8,4 +8,6 @@ const studentSchema = new mongoose.Schema({
     password: { type: String, required: true },
 });
 
-export const Student = mongoose.model('Student', studentSchema);
+const Student = mongoose.model('Student', studentSchema);
+
+module.exports = Student;
